d86d4d3948008fd9781c82f7e37cd27b95eaa65a
Raymond <raymondaler2@gmail.com>
2023-12-04 20:44:39 +0800

    implemented get user location

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import NavBar from "./../components/NavBar.jsx";
 import CryptoJS from "crypto-js";
 import axios from "axios";
-import { useState, useEffect } from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { useState, useEffect, useRef } from "react";
+import { GoogleMap, MarkerF } from "@react-google-maps/api";
 import { Drawer, List, ListItem, ListItemText } from "@mui/material";
 
 const Home = () => {
-  const [markers, setMarkers] = useState([
-    { id: 1, position: { lat: 37.7749, lng: -122.4194 }, name: "Marker 1" },
-    // Add more markers as needed
-  ]);
-
-  //http://localhost:5000/api/user/stores
-  //make use effect to call all stores for markers
-
+  const [userLocation, setUserLocation] = useState(null);
+  const [mapLoaded, setMapLoaded] = useState(false);
+  const mapRef = useRef(null);
+  const MAP_MARKER =
+    "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z";
+  const [markers, setMarkers] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
   const token = localStorage.getItem("token") ?? "";
   const jwtSecret = import.meta.env.VITE_JWT_SECRET;
@@ -35,6 +33,34 @@ const Home = () => {
       console.error("Decode Token Error:", error.message);
       throw error;
     }
+  };
+
+  const FetchMarkers = async () => {
+    const result = await axios.get(`http://localhost:5000/api/user/stores`);
+    setMarkers(result.data);
+  };
+
+  const handleMarkerClick = (marker) => {
+    setSelectedMarker(marker);
+  };
+
+  const handleDrawerClose = () => {
+    setSelectedMarker(null);
+  };
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation({
+          lat: 10.352136269202115,
+          lng: 123.91327011184387,
+        });
+      },
+      (error) => {
+        console.error("Error getting current location:", error.message);
+      }
+    );
   };
 
   useEffect(() => {
@@ -69,13 +95,16 @@ const Home = () => {
     }
   }, [token]);
 
-  const handleMarkerClick = (marker) => {
-    setSelectedMarker(marker);
-  };
+  useEffect(() => {
+    if (!mapLoaded) {
+      setMapLoaded(true);
+    }
+  }, [mapLoaded]);
 
-  const handleDrawerClose = () => {
-    setSelectedMarker(null);
-  };
+  useEffect(() => {
+    getCurrentLocation();
+    FetchMarkers();
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
@@ -85,6 +114,12 @@ const Home = () => {
           mapContainerStyle={{ height: "100%", width: "100%" }}
           zoom={9}
           center={{ lat: 10.3157, lng: 123.8854 }}
+          onLoad={(map) => {
+            if (!mapRef.current) {
+              mapRef.current = map;
+              setMapLoaded(true);
+            }
+          }}
           options={{
             mapTypeId: "hybrid",
             streetViewControl: false,
@@ -93,15 +128,32 @@ const Home = () => {
             tilt: 0,
           }}
         >
-          {markers.map((marker) => (
-            <Marker
-              key={marker.id}
-              position={marker.position}
+          <MarkerF
+            key={"user_location"}
+            position={{
+              lat: userLocation?.lat,
+              lng: userLocation?.lng,
+            }}
+          />
+          {markers?.map((marker) => (
+            <MarkerF
+              icon={{
+                path: MAP_MARKER,
+                scale: 2,
+                fillColor: "rgb(4,156,228)",
+                fillOpacity: 1,
+                strokeColor: "hsla(52, 71%, 80%, 1)",
+                anchor: { x: 12, y: 24 },
+              }}
+              key={marker._id}
+              position={{
+                lat: marker.store_location.geometry.location.lat,
+                lng: marker.store_location.geometry.location.lng,
+              }}
               onClick={() => handleMarkerClick(marker)}
             />
           ))}
         </GoogleMap>
-
         <Drawer
           anchor="bottom"
           open={!!selectedMarker}
